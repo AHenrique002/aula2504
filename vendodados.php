a43d42cbc0d0c4a144612caeb59d38b05ed6bb94
AHenrique002 <henriqueprestes01@gmail.com>
2024-04-26 14:45:53 -0400

    Update vendodados.php

--- vendodados.php
+++ vendodados.php
@@ -1,4 +1,4 @@
-<<?php 
+<?php 
 require_once 'config.php';
 
 $senhasecreta = "123";
@@ -7,14 +7,12 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
 
 //Digitou a senha certo
 if($senhadigitada === $senhasecreta){
-    $sql = "SELECT * FROM mydados";
+    $sql = "SELECT * FROM alunos";
     $result = $conn->query($sql);
 }else {
     echo "<h1>Senha incorreta!</h1>";
 }
 }
-
-
 ?>
 
 <!DOCTYPE html>
@@ -24,30 +22,35 @@ if($senhadigitada === $senhasecreta){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="estilo.css">
+
 </head>
 <body>
-    <div>
-        <form method="post">
-            <label for="senha">Senha:</label>
-            <input type="password" name="senha" placeholder="Digite sua senha" required/>
-            <br/>
-            <button type="submit">Enviar</button>
+    <form method="post">
+    <label for="senha">Senha:</label>
+    <input type="password" name="senha" placeholder="Digite sua senha" required/>
+    <br/>
+    <button type="submit">Enviar</button>
+    </form>
 
-        </form>
-    </div class = "mensagens">
+<div class="mensagens">
 <?php if(isset($result) && $result ->num_rows >0) : ?>
     <h2>Dados Cadastrados</h2>  
     <ul>
         <?php while($row = $result->fetch_assoc()) : ?>
+            <br/>
             <li>
                 <strong>Nome:</strong> <?php echo $row["nome"];?><br/>
-                <strong>Email:</strong> <?php echo $row["email"];?><br/>
+                <strong>E-mail:</strong> <?php echo $row["email"];?><br/>
+                <strong>Contato:</strong> <?php echo $row["phone"];?><br/>
+                <strong>Genero:</strong> <?php echo $row["sexo"];?><br/>
+                <strong>Curso:</strong> <?php echo $row["curso"];?><br/>
                 <strong>Data e Hora:</strong> <?php echo $row["data"]."ás".$row["hora"];?><br/><br/>
             </li>
-            <?php endwhile;?>
+        <?php endwhile;?>
     </ul>
     <?php else: ?>
-        <p>Não há nenhuma Mensagem</p>
+        <p>Não há nenhuma mensagem</p>
         <?php endif?>
+</div>
 </body>
 </html>
